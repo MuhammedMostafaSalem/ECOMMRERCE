c36a08ec638a7be79ecfd1c288ed51d29f32b107
Muhammed Mostafa <marwansalem164@gmail.com>
2023-11-22 00:17:41 +0200

    Add CategoryReducer in RootReducer

--- frontend/src/Redux/Reducers/RootReducer.js
+++ frontend/src/Redux/Reducers/RootReducer.js
@@ -3,10 +3,12 @@ import AuthReducer from './Auth/AuthReducer';
 import ForgotPasswordReducer from './Auth/ForgotPasswordReducer';
 import ProfileReducer from './User/ProfileReducer';
 import UsersAdminReducer from "./Admin/UsersAdminReducer";
+import CategoryReducer from "./Category/CategoryReducer";
 
 export default combineReducers({
     AuthReducer: AuthReducer,
     ForgotPasswordReducer: ForgotPasswordReducer,
     ProfileReducer: ProfileReducer,
     UsersAdminReducer: UsersAdminReducer,
+    CategoryReducer: CategoryReducer,
 })
