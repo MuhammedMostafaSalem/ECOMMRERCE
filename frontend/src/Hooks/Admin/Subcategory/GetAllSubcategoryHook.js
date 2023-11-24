0b5d1880968d4ad1a64ca2b99f24be358b261e03
Muhammed Mostafa <marwansalem164@gmail.com>
2023-11-24 22:15:49 +0200

    Create GetAllSubcategoryHook

---
+++ frontend/src/Hooks/Admin/Subcategory/GetAllSubcategoryHook.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSubcategory } from "../../../Redux/Actions/Subcategory/SubcategoryAction";
+
+const GetAllSubcategoryHook = () => {
+    const dispatch = useDispatch();
+
+    const subcats = useSelector(state => state.SubcategoryReducer.allSubcategory);
+    const loading = useSelector(state => state.SubcategoryReducer.loading);
+
+    useEffect(() => {
+        if(subcats) {
+            dispatch(getAllSubcategory())
+        }
+    }, [])
+
+    let allSubcat = []
+    try {
+        if(subcats.data.data) {
+            allSubcat = subcats.data.data;
+        }
+        else {
+            allSubcat = []
+        }
+    } catch(e) {}
+
+    return [allSubcat, loading]
+}
+
+export default GetAllSubcategoryHook
