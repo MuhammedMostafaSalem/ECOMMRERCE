b0d0b16b5c338bc4755c9aa708966e115d5b465d
Muhammed Mostafa <marwansalem164@gmail.com>
2023-11-24 22:15:03 +0200

    Add getAllSubcategory in SubcategoryAction

--- frontend/src/Redux/Actions/Subcategory/SubcategoryAction.js
+++ frontend/src/Redux/Actions/Subcategory/SubcategoryAction.js
@@ -1,5 +1,6 @@
 import { useCreateData } from "../../../Hook/useCreateData";
-import { CREATE_SUBCATEGORY, GET_ERROR } from "../../Types";
+import { useGetData } from "../../../Hook/useGetData";
+import { CREATE_SUBCATEGORY, GET_ALL_SUBCATEGORY, GET_ERROR } from "../../Types";
 
 // create subcategory
 export const createSubategory = (formData) => async (dispatch) => {
@@ -16,5 +17,22 @@ export const createSubategory = (formData) => async (dispatch) => {
             type: GET_ERROR,
             payload: "Error " + err,
         })
+    }
+}
+
+// get all subcategory
+export const getAllSubcategory = () => async(dispatch) => {
+    try {
+        const response = await useGetData(`/api/v1/subcategories`);
+
+        dispatch({
+            type: GET_ALL_SUBCATEGORY,
+            payload: response,
+        });
+    } catch(error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: 'Error ' + error,
+        });
     }
 }
