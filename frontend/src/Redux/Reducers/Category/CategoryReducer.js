b7cb3f3bf5a493967eadae826e30969a5b6c38cd
Muhammed Mostafa <marwansalem164@gmail.com>
2023-11-24 02:23:43 +0200

    Add updateCategory in CategoryReducer

--- frontend/src/Redux/Reducers/Category/CategoryReducer.js
+++ frontend/src/Redux/Reducers/Category/CategoryReducer.js
@@ -1,7 +1,8 @@
-import { GET_ALL_CATEGORIES } from "../../Types";
+import { GET_ALL_CATEGORIES, UPDATE_CATEGORY } from "../../Types";
 
 const initail = {
     allCategories: [],
+    updateCategory: [],
     loading: true,
 }
 
@@ -11,6 +12,12 @@ const CategoryReducer = (state=initail, action)  => {
             return {
                 ...state,
                 allCategories: action.payload,
+                loading: false,
+            }
+        case UPDATE_CATEGORY:
+            return {
+                ...state,
+                updateCategory: action.payload,
                 loading: false,
             }
         default: 
