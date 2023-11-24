cddde7afd412bd74ab76b4587a0ff5ef1704fb8b
Muhammed Mostafa <marwansalem164@gmail.com>
2023-11-24 22:36:15 +0200

    Create CustomHook( DeleteSubcategoryAdminHook )

---
+++ frontend/src/Hooks/Admin/Subcategory/DeleteSubcategoryAdminHook.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteSubcategory } from "../../../Redux/Actions/Subcategory/SubcategoryAction";
+import { toast } from "react-toastify";
+
+const DeleteSubcategoryAdminHook = () => {
+    const dispatch = useDispatch();
+
+    const [idSubcatDelete, setIdSubcatDelete] = useState('');
+    const [showDeleteSubcat, setShowDeleteSubcat] = useState(false);
+
+    const handleCloseDeleteSubcat = () => setShowDeleteSubcat(false);
+    const handleShowDeleteSubcat = (id) => {
+        console.log(id)
+        setIdSubcatDelete(id)
+        setShowDeleteSubcat(true);
+    }
+
+    const deleteSubcatHandler = async (id) => {
+        await dispatch(deleteSubcategory(id))
+        toast('Subcategory Deleted Successfully');
+        handleCloseDeleteSubcat();
+    }
+
+    return [
+        idSubcatDelete,
+        showDeleteSubcat,
+        handleCloseDeleteSubcat,
+        handleShowDeleteSubcat,
+        deleteSubcatHandler,
+    ]
+}
+
+export default DeleteSubcategoryAdminHook
