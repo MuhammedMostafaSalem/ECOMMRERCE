24bf3a1634354da80945854592429bb4fabde906
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-02 10:17:15 +0200

    Add deleteProduct

--- frontend/src/Redux/Actions/Product/ProductAction.js
+++ frontend/src/Redux/Actions/Product/ProductAction.js
@@ -1,6 +1,7 @@
 import { useCreateDataWithImg } from "../../../Hook/useCreateData";
+import useDeleteData from "../../../Hook/useDeleteData";
 import { useGetData } from "../../../Hook/useGetData";
-import { CREATE_PRODUCT, GET_ALL_PRODUCTS, GET_ERROR } from "../../Types";
+import { CREATE_PRODUCT, DELETE_PRODUCT, GET_ALL_PRODUCTS, GET_ERROR } from "../../Types";
 
 //create product
 export const createProduct = (formData) => async (dispatch) => {
@@ -36,3 +37,21 @@ export const getAllProducts = () => async(dispatch) => {
         });
     }
 }
+
+// delete product
+export const deleteProduct = (id) => async (dispatch) => {
+    try {
+        const response = await useDeleteData(`/api/v1/product/${id}`);
+
+        dispatch({
+            type: DELETE_PRODUCT,
+            payload: response,
+            loading: true,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ERROR,
+            payload: "Error " + error
+        });
+    }
+};
