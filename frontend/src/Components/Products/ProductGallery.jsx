93e600984f58a478e93c72e9c4cb1b3abd09761d
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:44:48 +0200

    Add images

--- frontend/src/Components/Products/ProductGallery.jsx
+++ frontend/src/Components/Products/ProductGallery.jsx
@@ -2,21 +2,8 @@ import React from 'react'
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 
-const ProductGallery = () => {
-    const images = [
-        {
-            original: "https://picsum.photos/id/1018/1000/600/",
-            thumbnail: "https://picsum.photos/id/1018/250/150/",
-        },
-        {
-            original: "https://picsum.photos/id/1015/1000/600/",
-            thumbnail: "https://picsum.photos/id/1015/250/150/",
-        },
-        {
-            original: "https://picsum.photos/id/1019/1000/600/",
-            thumbnail: "https://picsum.photos/id/1019/250/150/",
-        },
-    ];
+const ProductGallery = ({images}) => {
+
 
     return (
         <div className='product-gallary-card'>
