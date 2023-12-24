e7a62f86ee5a1628ab5b546143459fd27179ffd9
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:48:32 +0200

    Add name and image

--- frontend/src/Components/Category/CategoryCard.jsx
+++ frontend/src/Components/Category/CategoryCard.jsx
@@ -4,25 +4,25 @@ import { SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 import { Col } from 'react-bootstrap';
 
-const CategoryCard = () => {
+const CategoryCard = ({name, image}) => {
     return (
         <Col xs="4" sm="3" md="2" lg="2" className="m-3">
             <div className="allCard mb-3">
                 <div className="categoty-card"></div>
-                <img alt="zcv" src={itemImg} className="categoty-card-img" />
-                <p className="categoty-card-text">category name</p>
+                <img alt="zcv" src={image} className="categoty-card-img" />
+                <p className="categoty-card-text text-center">{name}</p>
             </div>
         </Col>
     )
 }
 
-const CategoryCardSplide = () => {
+const CategoryCardSplide = ({name, image}) => {
     return (
         <SplideSlide>
             <div className='card'>
                 <div className='categoty-card'></div>
-                <img alt="zcv" src={itemImg}/>
-                <p className='categoty-card-text'>category name</p>
+                <img alt="zcv" src={image}/>
+                <p className='categoty-card-text'>{name}</p>
             </div>
         </SplideSlide>
     )
