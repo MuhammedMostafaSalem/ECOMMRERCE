dbaae382ead817f62f033e4d9bb9d17fed86be0b
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:48:11 +0200

    Add ViewHomeCatsHook

--- frontend/src/Components/Category/CategoryContainer.jsx
+++ frontend/src/Components/Category/CategoryContainer.jsx
@@ -3,50 +3,46 @@ import { CategoryCard, CategoryCardSplide } from './CategoryCard'
 import { Splide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 import { Row } from 'react-bootstrap';
+import GetAllCategoryHook from '../../Hooks/Admin/Category/GetAllCategoryHook';
+import ViewHomeCatsHook from '../../Hooks/Admin/Category/ViewHomeCatsHook';
 
 const CategoryContainer = () => {
+    const [itemsCategory] = ViewHomeCatsHook();
+    
     return (
-        // <Row className='divContainer categoryCard'>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        //     <CategoryCard/>
-        // </Row>
-        <div className='divContainer categoryContainer'>
-            <Splide
-                aria-label="My Favorite Images"
-                options = {
-                    {
-                        type: 'loop',
-                        fixedWidth: '130px',
-                        focus: 'center',
-                        isNavigation: true,
-                        autoplay: true,
-                        gap: '10px',
-                    }
-                }
-            >
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-                <CategoryCardSplide />
-            </Splide>
+        <div>
+            {
+                itemsCategory.length <= 10 ?
+                    <Row className='divContainer categoryCard'>
+                        {
+                            itemsCategory.map((item, index) => (
+                                <CategoryCard name={item.name} image={item.image} key={index} />
+                            ))
+                        }
+                    </Row>
+                :
+                <div className='divContainer categoryContainer'>
+                    <Splide
+                        aria-label="My Favorite Images"
+                        options = {
+                            {
+                                type: 'loop',
+                                fixedWidth: '130px',
+                                focus: 'center',
+                                isNavigation: true,
+                                autoplay: true,
+                                gap: '10px',
+                            }
+                        }
+                    >
+                        {
+                            itemsCategory.map((item, index) => (
+                                <CategoryCardSplide key={index} name={item.name} image={item.image} />
+                            ))
+                        }
+                    </Splide>
+                </div>
+            }
         </div>
         )
 }
