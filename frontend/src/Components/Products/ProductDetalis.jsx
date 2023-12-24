bb360592e44cbf82e8340e1602689cdfb52fb063
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:45:49 +0200

    Add CustomHook( ViewProductsDetalisHook ) and params ID

--- frontend/src/Components/Products/ProductDetalis.jsx
+++ frontend/src/Components/Products/ProductDetalis.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import { Col, Row } from 'react-bootstrap';
 import ProductGallery from './ProductGallery';
 import ProductText from './ProductText';
+import { useParams } from 'react-router-dom';
+import ViewProductsDetalisHook from '../../Hooks/Admin/Product/ViewProductsDetalisHook';
 
 const ProductDetalis = () => {
+    const {id} = useParams();
+    const [prodItem, images, cat]= ViewProductsDetalisHook(id);
+
     return (
-        <div style={{minHeight: '593px'}}>
+        <div className='my-5' style={{minHeight: '593px'}}>
             <div className='divContainer'>
                 <Row>
                     <Col md='6'>
-                        <ProductGallery />
+                        <ProductGallery images={images} />
                     </Col>
                     <Col md='6'>
-                        <ProductText />
+                        <ProductText prodItem={prodItem} cat={cat} />
                     </Col>
                 </Row>
             </div>
