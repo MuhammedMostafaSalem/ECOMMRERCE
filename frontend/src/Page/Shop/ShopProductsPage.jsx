dbc13cc34b68dde99093f28b90798212afc3bbf1
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:34:10 +0200

    Add ViewSearchProductsHook

--- frontend/src/Page/Shop/ShopProductsPage.jsx
+++ frontend/src/Page/Shop/ShopProductsPage.jsx
@@ -4,8 +4,11 @@ import { Col, Row } from 'react-bootstrap';
 import SideFilter from '../../Components/Utilities/SideFilter';
 import CardProductsContainer from '../../Components/Products/CardProductsContainer';
 import Pagination from './../../Components/Utilities/Pagination/Pagination';
+import ViewSearchProductsHook from '../../Hooks/Admin/Product/ViewSearchProductsHook';
 
 const ShopProductsPage = () => {
+const [items, pagination, onPress, getProducts] = ViewSearchProductsHook();
+
     return (
         <div style={{ minHeight: '593px'}}>
             <div className='divContainer'>
@@ -15,8 +18,12 @@ const ShopProductsPage = () => {
                     </Col>
                     <Col lg='10' md='9' sm='8' className='sideProducts'>
                         <h4 className="productsHeading">products</h4>
-                        <CardProductsContainer />
-                        <Pagination />
+                        <CardProductsContainer products={items} />
+                        {
+                            pagination > 1 ?
+                                <Pagination pageCount={pagination} onPress={onPress} />
+                            : null
+                        }
                     </Col>
                 </Row>
             </div>
