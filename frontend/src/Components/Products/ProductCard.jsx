5c458c75609fa38e2605f4e0ba352b1b6a7d8e2d
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:46:26 +0200

    Add nam and price and image, id

--- frontend/src/Components/Products/ProductCard.jsx
+++ frontend/src/Components/Products/ProductCard.jsx
@@ -5,20 +5,20 @@ import prod1 from '../../images/prod1.png'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion';
 
-const ProductCard = () => {
+const ProductCard = ({name, price, image, id}) => {
     return (
         <Col lg='3' md='4' sm='6' xs='12'>
             <motion.div whileHover={{scale: 0.9}}>
                 <Card className='my-2' style={{border: 'none'}}>
-                    <Link className='productID' to='/product/:id'>
-                        <Card.Img variant="top" src={prod1} />
+                    <Link className='productID' to={`/product/${id}`}>
+                        <Card.Img variant="top" src={image} />
                     </Link>
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>{name}</Card.Title>
                         <div className='rait'>
                             star (reviews)
                         </div>
-                        <div className='price'>$ 123</div>
+                        <div className='price'>$ {price}</div>
                     </Card.Body>
                 </Card>
             </motion.div>
