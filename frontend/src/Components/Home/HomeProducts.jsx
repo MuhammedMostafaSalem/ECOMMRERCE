3560a25112535be5247ee81b4da5be6266ae07cf
Muhammed Mostafa <marwansalem164@gmail.com>
2023-12-24 14:47:33 +0200

    Add ViewHomeProductsHook

--- frontend/src/Components/Home/HomeProducts.jsx
+++ frontend/src/Components/Home/HomeProducts.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CardProductsContainer from '../Products/CardProductsContainer'
+import ViewHomeProductsHook from '../../Hooks/Admin/Product/ViewHomeProductsHook';
 
 const HomeProducts = () => {
+    const [itemsProduct] = ViewHomeProductsHook();
+    
     return (
         <div className='homeProducts'>
             <p>featured products</p>
-            <CardProductsContainer/>
+            <CardProductsContainer products={itemsProduct} />
         </div>
     )
 }
