55d148f0d49880bdc14c3ee48995d9b7c65800dc
Muhammed Mostafa <marwansalem164@gmail.com>
2024-01-04 17:39:15 +0200

    Add CustomHook(ViewProductsDetalisHook) and useParams

--- frontend/src/Page/Shop/ProductDetailsPage.jsx
+++ frontend/src/Page/Shop/ProductDetailsPage.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ProductDetalis from '../../Components/Products/ProductDetalis'
+import ReviewContainer from '../../Components/Reviews/ReviewContainer'
+import { useParams } from 'react-router-dom';
+import ViewProductsDetalisHook from '../../Hooks/Admin/Product/ViewProductsDetalisHook';
 
 const ProductDetailsPage = () => {
+    const {id} = useParams();
+    const [prodItem, images, cat]= ViewProductsDetalisHook(id);
+    let prodReview = []
+    try {
+        if(prodItem.reviews) {
+            prodReview = prodItem.reviews;
+        }
+        else {
+            prodReview = []
+        }
+    } catch(e) {}
+
     return (
         <div>
-            <ProductDetalis />
+            <ProductDetalis id={id} prodItem={prodItem} images={images} cat={cat} />
+            <ReviewContainer prodReview={prodReview} />
         </div>
     )
 }
