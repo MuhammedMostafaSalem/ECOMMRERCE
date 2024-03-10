c881d5aa49ee780433ee88549734397084320a88
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-10 07:35:14 +0200

    Add checkOutHandler

--- frontend/src/Components/Cart/CartCheckout.jsx
+++ frontend/src/Components/Cart/CartCheckout.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const CartCheckout = ({totalCartPrice}) => {
+    const navigate = useNavigate();
+    const checkOutHandler = () => {
+        navigate("/shipping")
+    }
     return (
         <div className='cartCheckout'>
             <table>
@@ -8,7 +13,7 @@ const CartCheckout = ({totalCartPrice}) => {
                     <td>total price</td>
                     <td>$ {totalCartPrice}</td>
                 </tr>
-                <button className="cart-checkout">check out</button>
+                <button className="cart-checkout" onClick={checkOutHandler}>check out</button>
             </table>
         </div>
     )
