7e8faf63dca363d33c97b021fb83bf3d49ea6bb3
Muhammed Mostafa <marwansalem164@gmail.com>
2024-03-10 08:34:38 +0200

    Update backend

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
+// Config
+if(process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "./config/.env" });
+}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -44,7 +48,11 @@ app.use('/api/v1', order);
 app.use('/api/v1/cart', cart);
 
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+app.get("*", (req, rea) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+})
 
 // Middleware for Errors
 app.use(errorMiddleware)
