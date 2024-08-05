3c1507504139fcc3a6f0cc831324a114677de8bc
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-06 00:33:45 +0300

    word localhost

--- frontend/src/Api/baseURL.js
+++ frontend/src/Api/baseURL.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const baseURL = axios.create({baseURL: 'http://localhost:8000'});
-const baseURL = axios.create({baseURL: 'https://ecommrerce.vercel.app'});
+const baseURL = axios.create({baseURL: 'http://localhost:8000'});
+// const baseURL = axios.create({baseURL: 'https://ecommrerce.vercel.app'});
 
 export default baseURL
