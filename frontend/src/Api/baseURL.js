e474ca58fb4abf692bbda69ea818debb655e91e4
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-06 22:33:23 +0300

    Update baseURL

--- frontend/src/Api/baseURL.js
+++ frontend/src/Api/baseURL.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = axios.create({baseURL: 'http://127.0.0.1:8000'});
-const baseURL = axios.create({baseURL: 'http://localhost:8000'});
+// const baseURL = axios.create({baseURL: 'http://localhost:8000'});
+const baseURL = axios.create({baseURL: 'https://ecommrerce.vercel.app'});
 
 export default baseURL
