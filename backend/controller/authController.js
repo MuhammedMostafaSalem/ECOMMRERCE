7a3b00636fe770ee5ea20552e3f9b7506fd2a7d0
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-07 02:43:47 +0300

    Update resizeImage

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -10,6 +10,8 @@ const asyncHandler = require('express-async-handler');
 const {v4: uuidv4} = require("uuid");
 const sharp = require("sharp");
 const multer = require('multer');
+const path = require("path");
+const fs = require("fs");
 
 
 // exports.uploadUserImage = uploadSingleImage('image');
@@ -45,12 +47,30 @@ exports.uploadUserImage = upload.fields([
 ]);
 
 exports.resizeImage = asyncHandler(async (req, res, next) => {
+    // if (req.files.avatar) {
+    //     const ext = req.files.avatar[0].mimetype.split('/')[1];
+    //     const avatarFilename = `user-${uuidv4()}-${Date.now()}-cover.${ext}`;
+
+    //     await sharp(req.files.avatar[0].buffer)
+    //         .toFile(`uploads/users/${avatarFilename}`); // write into a file on the disk
+
+    //     // Save imageCover into database
+    //     req.body.avatar = avatarFilename;
+    // }
+
+    // next();
     if (req.files.avatar) {
         const ext = req.files.avatar[0].mimetype.split('/')[1];
         const avatarFilename = `user-${uuidv4()}-${Date.now()}-cover.${ext}`;
+        const uploadPath = path.join(__dirname, 'uploads', 'users');
+
+        // Ensure the directory exists
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
 
         await sharp(req.files.avatar[0].buffer)
-            .toFile(`uploads/users/${avatarFilename}`); // write into a file on the disk
+            .toFile(path.join(uploadPath, avatarFilename)); // write into a file on the disk
 
         // Save imageCover into database
         req.body.avatar = avatarFilename;
