7c288a397f9663934594e452989d9955bc371970
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2024-08-09 03:45:16 +0300

    Update baseURL

--- frontend/src/Api/baseURL.js
+++ frontend/src/Api/baseURL.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = axios.create({baseURL: 'http://127.0.0.1:8000'});
-const baseURL = axios.create({baseURL: 'http://localhost:8000'});
+// const baseURL = axios.create({baseURL: 'http://localhost:8000'});
+const baseURL = axios.create({baseURL: 'https://disturbed-sisile-marwansalem-5f94227c.koyeb.app'});
 
 export default baseURL
